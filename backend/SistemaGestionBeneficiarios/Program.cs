8d163858e51b5ff28cd465f8de3374fa77ae98ea
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-01-27 23:00:56 -0500

    feat: project setup

---
+++ backend/SistemaGestionBeneficiarios/Program.cs
@@ -0,0 +1,57 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Sistema Gestión Beneficiarios API",
+        Version = "v1",
+        Description = "API para la gestión de beneficiarios.",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Carlo Rodrigo Diaz Portilla"
+        }
+    });
+});
+
+// CORS Configuration
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? new[] { "http://localhost:5173" } ;
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(allowedOrigins)
+              .AllowCredentials()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sistema Gestión Beneficiarios API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
