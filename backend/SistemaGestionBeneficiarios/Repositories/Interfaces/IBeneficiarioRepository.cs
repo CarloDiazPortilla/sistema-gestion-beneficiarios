b51369343180def30cef082c33f2849dec500f03
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-01-28 08:12:53 -0500

    feat: add repository interfaces

---
+++ backend/SistemaGestionBeneficiarios/Repositories/Interfaces/IBeneficiarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using SistemaGestionBeneficiarios.API.Models;
+using SistemaGestionBeneficiarios.API.DTOs;
+
+namespace SistemaGestionBeneficiarios.API.Repositories.Interfaces
+{
+    public interface IBeneficiarioRepository
+    {
+        Task<IEnumerable<Beneficiario>> ObtenerTodosAsync();
+        Task<Beneficiario?> ObtenerPorIdAsync(int id);
+        Task<Beneficiario> CrearAsync(BeneficiarioDto beneficiarioDto);
+        Task<Beneficiario?> ActualizarAsync(int id, BeneficiarioDto beneficiarioDto);
+        Task<bool> EliminarAsync(int id);
+    }
+}
---
+++ backend/SistemaGestionBeneficiarios/Repositories/Interfaces/IDocumentoIdentidadRepository.cs
@@ -0,0 +1,11 @@
+﻿using SistemaGestionBeneficiarios.API.Models;
+
+namespace SistemaGestionBeneficiarios.API.Repositories.Interfaces
+{
+    public interface IDocumentoIdentidadRepository
+    {
+        Task<IEnumerable<DocumentoIdentidad>> ObtenerTodosAsync();
+        Task<IEnumerable<DocumentoIdentidad>> ObtenerActivosAsync();
+        Task<DocumentoIdentidad?> ObtenerPorIdAsync(int id);
+    }
+}
